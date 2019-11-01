4ac19ceba425e52e291595c5192d79fb5803d864
khirshah <khirshah@gmail.com>
2019-11-01 17:14:35 +0000

    fetch multiple files with promise all

--- src/App.js
+++ src/App.js
@@ -5,42 +5,54 @@ import dogdata from "./dogdata";
 class App extends React.Component {
 
   state = {
-    jsonph: "",
-    github: ""
+    users: "",
+    todos: ""
   }
 
-  callRecusrion = () => {
-    let html = this.handleRecursion(dogdata,'');
+  //method to direct local data to the converter method
+  loadLocalData = () => {
+    let html = this.jsonToHTML(dogdata,'');
     return {__html: html };
   }
 
-  handleRecursion(obj,htmlString) {
+  //class method to convert json to a string containing html code
+  jsonToHTML = (obj,htmlString) => {
+    //open container div for object (or object part in deeper levels)
     htmlString += `<div class="container">`;
+    //iterate object's keys
     for (let i of Object.keys(obj)) {
+      //open container row div
       htmlString += `<div class="container-row">`;
+      //if current item is an object (and not jus null) - array is ok
       if (typeof obj[i] == "object" && obj[i]!=null) {
-        htmlString += `<div class="element">${i}:</div>`;
-        htmlString = this.handleRecursion(obj[i],htmlString);
+        //create an element div for the key
+        htmlString += `<div class="element">${i}</div>`;
+        //then call this funcion again and go to the deeper object level
+        htmlString = this.jsonToHTML(obj[i],htmlString);
       }
+      //if current item is a primitive type
       else {
-        htmlString += `<div class="element">${i}:</div> <div class="element">${obj[i]}</div>`;
+        //create an element div pair for key and value
+        htmlString += `<div class="element">${i}</div> <div class="element">${obj[i]}</div>`;
       }
+      //close container row div
       htmlString += "</div>";
     }
+    //end of for cycle close container div
     htmlString += "</div>";
+    //return the HTML string to either one level up, or back to the caller function when finished
     return htmlString
   }
 
+  //react lifecycle method where the json data is fetched
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users/1')
-      .then(response => {
-       if (!response.ok) {
-           throw new Error("HTTP error " + response.status);
-       }
-       return response.json();
-      })
-      .then(json => this.handleRecursion(json,""))
-      .then(html => this.setState({jsonph: html }));
+    Promise.all([
+      fetch('https://jsonplaceholder.typicode.com/todos/1'),
+      fetch('https://jsonplaceholder.typicode.com/users/1')
+    ])
+    .then(responses => Promise.all(responses.map(res => res.json())))
+    .then(jsons => jsons.map(i => this.jsonToHTML(i,"")))
+    .then(htmls => this.setState({todos: htmls[0], users: htmls[1] }));
   }
 
   render () {
@@ -48,11 +60,15 @@ class App extends React.Component {
       <div className="App">
         <div>
           <h3>Loading data from local file</h3>
-          <div dangerouslySetInnerHTML={this.callRecusrion()} />
+          <div dangerouslySetInnerHTML={this.loadLocalData()} />
         </div>
         <div>
-          <h3>Loading data from jsonplaceholder</h3>
-          <div dangerouslySetInnerHTML={{__html:this.state.jsonph}} />
+          <h3>Loading data from jsonplaceholder - /todos/1 endpoint</h3>
+          <div dangerouslySetInnerHTML={{__html:this.state.todos}} />
+        </div>
+        <div>
+          <h3>Loading data from jsonplaceholder - /users/1 endpoint</h3>
+          <div dangerouslySetInnerHTML={{__html:this.state.users}} />
         </div>
       </div>
     );
